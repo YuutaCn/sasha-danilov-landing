116eac09bb20255bcf8120ca993f41e18a875dd3
Yuuta Segawa <yuuta@192.168.100.5>
2022-11-08 20:21:33 +0300

    feat: add traking current time

--- src/js/components/player.js
+++ src/js/components/player.js
@@ -3,7 +3,7 @@ const player = document.querySelector('.player'),
   progressBar = document.querySelector('.player__info-nav-controls-progress'),
   pauseBtn = document.querySelector('.player__cover-pause'),
   presentTime = document.querySelector('.player__info-nav-time-present'),
-  fullTime = document.querySelector('.player__cover-pause'),
+  fullTime = document.querySelector('.player__info-nav-time-full'),
   volumeBtn = document.querySelector('.player__info-nav-controls-volume-btn'),
   volumeSelect = document.querySelector('.player__info-nav-controls-volume-select'),
   audio = document.querySelector('.player__audio'),
@@ -12,6 +12,17 @@ const player = document.querySelector('.player'),
 let playerStatus = false;
 let volumeStatus = true;
 
+// update volume
+volumeSelect.value = localStorage.getItem('volume');
+audio.volume = volumeSelect.value / 100;
+
+// save volume settings
+setInterval(() => {
+  if (volumeSelect.value > 5) {
+    localStorage.setItem('volume', volumeSelect.value);
+  };
+}, 1007);
+
 // Play/Pause compositions
 function playComposition() {
   if (playerStatus === false) {
@@ -19,12 +30,17 @@ function playComposition() {
     playerStatus = true;
     document.querySelector('.player__cover-pause-svg-play').classList.add('--disable');
     document.querySelector('.player__cover-pause-svg-pause').classList.remove('--disable');
+    document.querySelector('.player__cover-pause-svg-pause').classList.add('--active');
     changeOnList.classList.add('--active');
+    setTimeout(() => {
+      updateFullTime(fullTime, audio.duration);
+    }, 1000);
   } else {
     audio.pause();
     playerStatus = false;
     document.querySelector('.player__cover-pause-svg-play').classList.remove('--disable');
     document.querySelector('.player__cover-pause-svg-pause').classList.add('--disable');
+    document.querySelector('.player__cover-pause-svg-pause').classList.remove('--active');
   }
 }
 
@@ -37,6 +53,7 @@ changeOnList.addEventListener('click', playComposition);
 audio.addEventListener('timeupdate', function(e) {
   const { duration, currentTime } = e.srcElement;
   progressBar.value = (currentTime / duration) * 100;
+  updateFullTime(presentTime, audio.currentTime);
 
   if (currentTime >= duration) {
     playComposition()
@@ -78,8 +95,7 @@ volumeBtn.addEventListener('click', function(){
 
 // select volume
 volumeSelect.oninput = function(el) {
-  audio.volume = volumeSelect.value / 100
-  // console.log(volumeSelect.value / 100)
+  audio.volume = volumeSelect.value / 100;
   if (volumeSelect.value == 0) {
     volumeStatus = false;
     document.querySelector('.player__info-nav-controls-volume-btn-svg').innerHTML = `<use xlink:href="img/sprite.svg#sound-mute"></use>`;
@@ -89,13 +105,11 @@ volumeSelect.oninput = function(el) {
   };
 };
 
-// update volume
-volumeSelect.value = localStorage.getItem('volume');
-audio.volume = volumeSelect.value / 100;
-
-// save volume settings
-setInterval(() => {
-  if (volumeSelect.value > 5) {
-    localStorage.setItem('volume', volumeSelect.value);
-  };
-}, 1007);
+// time update
+function updateFullTime(el, time) {
+  let min10 = Math.floor(time / 600);
+  let min1 = Math.floor(time / 60);
+  let sec10 = Math.floor(time % 60 / 10);
+  let sec1 = Math.floor(time % 10);
+  el.innerHTML = `${min10}${min1}:${sec10}${sec1}`
+}
