7574985938a23ad565745f5f9b6395e796cf04ba
yuutacn <yuutacn@gmail.com>
2022-11-09 17:48:08 +0300

    feat: complite contact page

---
+++ src/js/components/page-checker.js
@@ -0,0 +1,12 @@
+if (document.getElementById('player')) {
+  document.querySelectorAll('.header__nav-list-item')[0].classList.toggle('--active');
+  document.querySelectorAll('.burger-menu__nav-list-item')[0].classList.toggle('--active');
+};
+if (document.getElementById('about')) {
+  document.querySelectorAll('.header__nav-list-item')[1].classList.toggle('--active');
+  document.querySelectorAll('.burger-menu__nav-list-item')[1].classList.toggle('--active');
+};
+if (document.getElementById('contact')) {
+  document.querySelectorAll('.header__nav-list-item')[2].classList.toggle('--active');
+  document.querySelectorAll('.burger-menu__nav-list-item')[2].classList.toggle('--active');
+};
